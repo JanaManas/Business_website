c1b78125630b0eccf89527bbbb41c8251fa4f751
MANAS JANA <165286574+JanaManas@users.noreply.github.com>
2026-05-03 00:36:34 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,13 +1,12 @@
 function validateForm() {
   let name = document.getElementById("name").value.trim();
   let email = document.getElementById("email").value.trim();
-  let message = document.getElementById("message").value.trim();
 
-  if (name === "" || email === "" || message === "") {
-    alert("Please fill all fields!");
+  if (name === "" || email === "") {
+    alert("Please fill all fields");
     return false;
   }
 
-  alert("✅ Message sent successfully!");
+  alert("Message sent successfully!");
   return true;
 }
